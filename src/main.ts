e9fbd31651178fa8c516d098a68bc87077630133
drumath <2237k.drumath@gmail.com>
2022-09-26 00:48:25 +0900

    :sparkles: canvasの用意など

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
 import './style.scss';
-import typescriptLogo from './typescript.svg';
-import { setupCounter } from './counter';
 
-document.querySelector<HTMLDivElement>('#app')?.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const main = (): void => {
+  const renderCanvas = document.getElementById(
+    'renderCanvas'
+  ) as HTMLCanvasElement;
+  if (renderCanvas == null) {
+    return;
+  }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+  renderCanvas.width = window.innerWidth;
+  renderCanvas.height = window.innerHeight;
+};
+
+main();

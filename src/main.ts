b53bdb42dc538bc7a4f9053503cebf26adc91c86
drumath <2237k.drumath@gmail.com>
2022-09-25 21:57:07 +0900

    :package: add eslint & prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './style.scss'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import './style.scss';
+import typescriptLogo from './typescript.svg';
+import { setupCounter } from './counter';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>('#app')?.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -18,8 +18,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-const a =1;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
